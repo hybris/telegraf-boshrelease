655bf299f98566a5771e6de866a068c5935e16b5
Pawel Oczadly <paweloczadly@gmail.com>
2016-12-22 10:36:54 +0100

    Concourse pipeline

---
+++ ci/bin/update_pipeline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+TARGET=${TARGET:-"hybris"}
+PIPELINE_NAME=${PIPELINE_NAME:-"telegraf-boshrelease"}
+
+if ! [ -x "$(command -v fly)" ]; then
+  echo 'fly is not installed.' >&2
+fi
+
+CREDENTIALS=$(mktemp /tmp/credentials.XXXXX)
+
+vault read -field=value -tls-skip-verify secret/idefix/concourse/$PIPELINE_NAME > ${CREDENTIALS}
+
+fly -t ${TARGET} set-pipeline -c pipeline.yml --load-vars-from=${CREDENTIALS} --pipeline=${PIPELINE_NAME}
+if [ $? -ne 0 ]; then
+  echo "Please login first: fly -t ${TARGET} login -k"
+fi
+
+rm -f ${CREDENTIALS}
