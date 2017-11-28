618e55b53e6753c54c3a928e3bd76410e2f7da36
mwieczorek <wieczorek-michal@wp.pl>
2017-11-28 15:29:20 +0100

    Changed pipelines to enable native concourse-vault integration

--- ci/bin/update_pipeline.sh
+++ ci/bin/update_pipeline.sh
@@ -9,13 +9,7 @@ if ! [ -x "$(command -v fly)" ]; then
   echo 'fly is not installed.' >&2
 fi
 
-CREDENTIALS=$(mktemp /tmp/credentials.XXXXX)
-
-vault read -format=json -tls-skip-verify secret/concourse/$PIPELINE_NAME | jq '.data' > ${CREDENTIALS}
-
-fly -t ${TARGET} set-pipeline -c pipeline.yml --load-vars-from=${CREDENTIALS} --pipeline=${PIPELINE_NAME}
+fly -t ${TARGET} set-pipeline -c pipeline.yml --pipeline=${PIPELINE_NAME}
 if [ $? -ne 0 ]; then
   echo "Please login first: fly -t ${TARGET} login -k"
 fi
-
-rm -f ${CREDENTIALS}
